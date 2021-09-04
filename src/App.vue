88e541358c0cae3bb39e83639f689bf7f65b8064
Delano van der Waal <d.g.d.van.der.waal@st.hanze.nl>
2021-09-04 16:35:14 +0200

    Fixed api calls axios, created ruleset get

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div>
+
+    </div>
 </template>
 
 <script lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+
 import {defineComponent} from "vue";
 import {Challenges} from "@/interfaces/policies";
+import {Ruleset} from "@/interfaces/ruleset";
 
 export default defineComponent({
     name: "App",
     components: {
-        HelloWorld,
+
     },
     data() {
         return {
-            challenges: {} as Challenges
+            challenges: {} as Challenges,
+            ruleset: {} as Ruleset
         }
     },
     mounted() {
         window.axios.get('https://raw.githubusercontent.com/KSneijders/AoE2-2v1/challenges/challenges/random/challenges.json')
-            .then(content => this.challenges = content)
+            .then(content => this.challenges = content as Challenges)
+        window.axios.get('https://raw.githubusercontent.com/KSneijders/AoE2-2v1/challenges/rulesets/default.json')
+            .then(content => this.ruleset = content as Ruleset)
+    },
+    methods: {
+
     }
 })
 
@@ -30,7 +40,7 @@ export default defineComponent({
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+    text-align: left;
     color: #2c3e50;
     margin-top: 60px;
 }
