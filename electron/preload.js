26583e5ef6675c460d8a41e3323f2701a9d852c5
KSneijders <k.sneijders@st.hanze.nl>
2021-09-04 15:37:20 +0200

    Added Axios and get request for challenges

--- electron/preload.js
+++ electron/preload.js
@@ -4,6 +4,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 // In this file we want to expose protected methods that allow the renderer
 // process to use the ipcRenderer without exposing the entire object.
-contextBridge.exposeInMainWorld("api", {
 
+// !!! REMEMBER !!!
+// ALL THE CONTEXT BRIDGE CONSTRUCTIONS NEED TO BE DEFINED IN: '/src/main.ts'
+contextBridge.exposeInMainWorld("axios", {
+    get: (url) => ipcRenderer.invoke('axios:get', url)
 });
