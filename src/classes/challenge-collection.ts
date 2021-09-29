389b6f7bd8cfd5583d568201f7015359ba1794c3
KSneijders <k.sneijders@st.hanze.nl>
2021-09-30 01:54:43 +0200

    Added limiter functionality to random challenges

--- src/classes/challenge-collection.ts
+++ src/classes/challenge-collection.ts
@@ -1,31 +1,38 @@
-import {Challenge, Challenges, PointObject, Points} from "@/interfaces/policies";
+import {CategoryPoints, Challenge, Challenges, PointObject, Points} from "@/interfaces/policies";
 import {randomPoints} from "@/scripts/points";
 import {sample, shuffle} from "@/scripts/arrays";
 import {jsonDeepCopy} from "@/scripts/other";
+import {Limiters} from "@/interfaces/limiters";
 
-interface IdMap {
+interface ChallengeIdMap {
     [key: string]: (string | number)[];
+}
+
+interface IgnoreIndexes {
+    [key: string]: number[];
 }
 
 class ChallengeCollection {
     private readonly maxIterations = 1000;
+    private readonly points: Points;
+    private readonly challengeIdMap: ChallengeIdMap;
 
-    challenges: Challenges;
-    private points: Points;
+    readonly challenges: Challenges;
+    readonly limiters: Limiters;
+
     private shuffled: boolean;
-    private readonly challengeIdMap: IdMap;
 
-    constructor(challenges: Challenges, shuffle = false) {
+    constructor(challenges: Challenges, limiters: Limiters, shuffle = false) {
+        this.limiters = limiters;
         this.challenges = jsonDeepCopy(challenges);
         this.challengeIdMap = {};
         this.points = randomPoints(25, 0);
         this.shuffled = shuffle;
 
-        this.initiateIdMap();
         if (shuffle) this.shuffleChallenges();
     }
 
-    initiateIdMap(): void {
+    resetChallengeIdMap(): void {
         for (const category of Object.keys(this.challenges)) {
             for (const [index, challenge] of this.challenges[category].entries()) {
                 this.challengeIdMap[challenge.id] = [category, index]
@@ -38,20 +45,28 @@ class ChallengeCollection {
             shuffle(this.challenges[key])
         }
         this.shuffled = true;
+        this.resetChallengeIdMap();
     }
 
     getRandomChallenges(): Challenge[] {
         if (!this.shuffled) throw Error("Cannot get random challenges without shuffling challenges.");
 
         let keys = Object.keys(this.points).filter(k => k != "wildPoints");
-        const pIndex = {economy: 0, building: 0, military: 0, miscellaneous: 0, technologies: 0} as Points
-        const challenges = [] as Challenge[]
+        const ignoreIndexes: IgnoreIndexes = {}
+        const challenges: Challenge[] = []
+
+        const indexProgress: CategoryPoints = {
+            economy: 0, building: 0, military: 0, miscellaneous: 0, technologies: 0,
+        }
+        for (const category of Object.keys(this.challenges)) {
+            ignoreIndexes[category] = []
+        }
 
         for (let i = 0; i <= this.maxIterations; i++) {
             const key = sample(keys);
             if (key === undefined) break;
 
-            const challenge = this.chooseFirstAllowedChallenge(key, pIndex);
+            const challenge = this.chooseFirstAllowedChallenge(key, ignoreIndexes, indexProgress);
             if (challenge === undefined) {
                 keys = keys.filter(k => k != key);
                 continue;
@@ -65,22 +80,33 @@ class ChallengeCollection {
         return challenges;
     }
 
-    private chooseFirstAllowedChallenge(key: string, pIndex: Points): Challenge | undefined {
+    private chooseFirstAllowedChallenge(key: string, ignoreIndexes: IgnoreIndexes, indexProgress: CategoryPoints): Challenge | undefined {
         const spendable = this.getSpendable(key);
 
-        for (let i = pIndex[key]; i < this.challenges[key].length; i++) {
-            const challenge = this.challenges[key][pIndex[key]];
-            pIndex[key]++;
+        for (let i = indexProgress[key]; i < this.challenges[key].length; i++) {
+            if (ignoreIndexes[key].includes(i)) continue
+            indexProgress[key] = i;
 
+            const challenge = this.challenges[key][indexProgress[key]];
             let cost: number;
             if (typeof challenge.points === "object") {
-                cost = challenge.points[this.selectRandomOption(key, challenge)]
+                const selectedOption = this.selectRandomOption(key, challenge)
+                if (selectedOption === undefined) continue
+                cost = challenge.points[selectedOption]
             } else {
                 cost = challenge.points
             }
 
             if (cost <= spendable) {
                 const wildPointCost = Math.min(Math.floor(cost / 2), this.points['wildPoints']);
+
+                for (const limiterElement of this.limiters[challenge.id] || []) {
+                    const mapped = this.challengeIdMap[limiterElement];
+                    if (mapped === undefined) throw Error(`Limiter ${limiterElement} does not exist in challenges`);
+
+                    const [cat, index] = mapped
+                    if (typeof index === "number") ignoreIndexes[cat].push(index)
+                }
 
                 this.points['wildPoints'] -= wildPointCost;
                 this.points[key] -= (cost - wildPointCost);
