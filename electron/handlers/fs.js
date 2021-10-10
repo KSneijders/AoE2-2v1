eb83906f22af14249a6e95e3c9920783f3ed64f8
KSneijders <k.sneijders@st.hanze.nl>
2021-10-11 01:20:04 +0200

    Added (wip) functions for profile control

--- electron/handlers/fs.js
+++ electron/handlers/fs.js
@@ -1,9 +1,10 @@
 const {ipcMain} = require('electron');
 const fs = require('fs');
 
+const FOLDERS = ['profiles'];
 const USER_PROFILE = process.env.USERPROFILE;
 const ROOT_2V1_PATH = `${USER_PROFILE}\\aoe2-2v1\\`
-const FOLDERS = ['profiles'];
+const PROFILES_PATH = `${ROOT_2V1_PATH}\\profiles\\`
 
 function verifyCreateFolders() {
     if (!fs.existsSync(ROOT_2V1_PATH)) fs.mkdirSync(ROOT_2V1_PATH);
@@ -13,41 +14,94 @@ function verifyCreateFolders() {
     }
 }
 
+/** *************************************************************************
+ *                           Profile Functions
+ * *************************************************************************/
+
+function readProfileContent(name) {
+    let response = fs.readFileSync(`${PROFILES_PATH + name}.json`, 'utf8');
+    return JSON.parse(response);
+}
+
+function getProfile(name) {
+    if (profileExists(name)) {
+        try {
+            return readProfileContent(name);
+        } catch (e) {
+            return {'response': e};
+        }
+    }
+    return {'response': `profile ${name} not found.`}
+}
+
+function profileExists(name) {
+    return fs.existsSync(`${PROFILES_PATH + name}.json`)
+}
+
+function createProfile(name, points) {
+    if (!profileExists(name)) {
+        let profileData = {
+            "name": name,
+            "points": points
+        };
+        return writeProfile(name, profileData)
+    } else {
+        return false;
+    }
+}
+
+function writeProfile(name, profile) {
+    try {
+        fs.writeFileSync(`${PROFILES_PATH + name}.json`, JSON.stringify(profile));
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+function p(callable) {
+    return new Promise(resolve => resolve(callable()));
+}
+
+/** *************************************************************************
+ *                          ipcMain Event Functions
+ * *************************************************************************/
+
 ipcMain.handle('fs:getProfile', (_, name) => {
     verifyCreateFolders();
 
-    const PROFILES_PATH = `${ROOT_2V1_PATH}\\profiles\\`
-
-    if (!fs.existsSync(`${PROFILES_PATH + name}.json`)) {
-        let profileData = {
-            "name": name,
-            "points": 25
-        }
-        fs.appendFileSync(`${PROFILES_PATH + name}.json`, JSON.stringify(profileData));
-    }
-
-    return new Promise(function (resolve) {
-        try {
-            let response = fs.readFileSync(`${PROFILES_PATH + name}.json`, 'utf8');
-                resolve(JSON.parse(response))
-        } catch (e) {
-            resolve({'response': e})
-        }
+    return p(() => {
+        createProfile(name, 25);
+        return getProfile(name)
     });
 })
 
+ipcMain.handle('fs:editProfile', (_, name, key, value) => {
+    verifyCreateFolders();
+
+    return p(() => {
+        if (profileExists(name)) {
+            const profile = getProfile(name);
+            profile[key] = value;
+            writeProfile(name, profile);
+
+            return true;
+        } else {
+            return false;
+        }
+    });
+});
 
 ipcMain.handle('fs:getProfileNames', () => {
-    const PROFILES_PATH = `${ROOT_2V1_PATH}\\profiles\\`
+    verifyCreateFolders();
 
     let files = fs.readdirSync(PROFILES_PATH);
     let profileNames = [];
-    for(let index in files){
+    for (let index in files) {
         profileNames.push(files[index].split('.json')[0]);
     }
     return profileNames
 });
-
 
 ipcMain.handle('fs:getProfiles', () => {
 
