b03f27289238405e1e53e84c69829cb55934e668
KSneijders <k.sneijders@st.hanze.nl>
2021-10-30 19:50:10 +0200

    Added disableMainProcessTS setting

--- vue.config.js
+++ vue.config.js
@@ -30,9 +30,13 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
             // Use this to change the entrypoint of your app's main process
-            mainProcessFile: 'electron/main.js',
+            mainProcessFile: './electron/main.js',
             // Use this to change the entry point of your app's render process. default src/[main|index].[js|ts]
-            rendererProcessFile: 'src/main.ts',
+            rendererProcessFile: './src/main.ts',
+            // Manually disable typescript plugin for main process.
+            // Enable if you want to use regular js for the main process (src/background.js by default).
+            disableMainProcessTypescript: true,
+
             builderOptions: {
                 productName: 'AoE2 2v1',
                 extraResources: [
