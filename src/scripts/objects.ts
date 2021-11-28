a619f0af95bd3b65d29a0d1918990b98b7f24de9
KSneijders <k.sneijders@st.hanze.nl>
2021-11-29 00:53:45 +0100

    Fixed accidental deletion of GenericObject

--- src/scripts/objects.ts
+++ src/scripts/objects.ts
@@ -1,3 +1,5 @@
+import {GenericObject} from "@/interfaces/objects";
+
 /**
  * Get a value based on the object and key. If the key is not present, create it with the default value.
  *
@@ -5,8 +7,6 @@
  * @param key The key to look for the value
  * @param default_ The value to set when the key isn't present
  */
-import {GenericObject} from "@/interfaces/object";
-
 function getDefault<T>(obj: Record<string, T>, key: string, default_: T): T {
     if (!(key in obj)) obj[key] = default_
     return obj[key]
